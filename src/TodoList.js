1db5a9be11605ea4156dc8ab8ddb49ceb4d05164
lml <soalin228@163.com>
2019-07-27 00:04:35 +0800

    初始化

---
+++ src/TodoList.js
@@ -0,0 +1,17 @@
+import React, {Component, Fragment} from 'react'
+
+class TodoList extends Component {
+  render () {
+    return (
+      <Fragment>
+        <input/>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+        </ul>
+      </Fragment>
+    )
+  }
+}
+
+export default TodoList
