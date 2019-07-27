165c0870cf10e47a14964d91c603b942cf944517
lml <soalin228@163.com>
2019-07-27 10:34:49 +0800

    响应式数据

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,29 @@
 import React, {Component, Fragment} from 'react'
 
 class TodoList extends Component {
+
+  // constructor 在组件创建时执行
+  constructor (props) {
+    super(props)
+    // 数据写在state中
+    this.state = {
+      inputValue: 'hello world',
+      list: []
+    }
+  }
+
+  handleInputChange (e) {
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+
   render () {
     return (
       <Fragment>
-        <input/>
+        <input value={this.state.inputValue}
+               onChange={this.handleInputChange.bind(this)}
+        />
         <ul>
           <li>1</li>
           <li>2</li>
