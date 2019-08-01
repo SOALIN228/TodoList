9ebbe6968028af8795095e8962e0f6d6dc70d033
lml <soalin228@163.com>
2019-08-01 20:31:40 +0800

    使用antd

--- README.md
+++ README.md
@@ -20,25 +20,33 @@
 
 9. ul中的遍历看着很复杂，可以封装成一个方法，使用JSX进行解析，方便阅读
 
-10. JSX 中的注释 {/* 这是注释 */}
+10. style 写法
 
-11. 在JSX中的所有内容都拿 {} 包一下就好，方便解析
+    ```react
+    style={{ marginTop: '10px', marginLeft: '10px' }} // 两种写法
+    
+    style={{ marginTop: 10, width: 300 }}
+    ```
 
-12. 使用dangerouslySetInnerHTML={{__html: value}} 可以禁止将标签进行转义，按照html格式进行解析
+11. JSX 中的注释 {/* 这是注释 */}
 
-13. 我们可以将组件进行拆分，方便复用
+12. 在JSX中的所有内容都拿 {} 包一下就好，方便解析
 
-14. 父子通信
+13. 使用dangerouslySetInnerHTML={{__html: value}} 可以禁止将标签进行转义，按照html格式进行解析
+
+14. 我们可以将组件进行拆分，方便复用
+
+15. 父子通信
     - 父组件将数据和方法传递给子组件，子组件通过this.props接收父组件传递的值
     - 父组件传递方法中的this，应该指向父组件，因为数据的具体操作应该由父组件控制，避免在子组件中修改父组件的值
 
-15. render函数在创建时会执行一次，每次state或props改变时，再次执行render进行页面渲染，从而实现数据驱动页面
+16. render函数在创建时会执行一次，每次state或props改变时，再次执行render进行页面渲染，从而实现数据驱动页面
 
-16. 通过ref来操作DOM
+17. 通过ref来操作DOM
     - 在html标签上，获取的是DOM节点 `ref={(button) => {this.buttonElem = button}}` 然后通过this.buttonElem 就可以获取DOM
     - 在组件上，获取的是js实例
 
-17. setState 是异步的
+18. setState 是异步的
     ```javascript
     this.setState = { // 没有办法在数据更新后执行逻辑
       counter
@@ -63,7 +71,7 @@
     })
     ```
 
-18. PropTypes 类型检查
+19. PropTypes 类型检查
 
     ```react
     import PropTypes from 'prop-types' // 引入
@@ -80,7 +88,7 @@
     }
     ```
 
-19. 生命周期函数指的是组件在某一时刻会自动执行的函数
+20. 生命周期函数指的是组件在某一时刻会自动执行的函数
 
     **挂载**
 
@@ -150,9 +158,9 @@
 
        在后代组件抛出错误后被调用
 
-20. 使用charles模拟请求的返回数据，做数据mock
+21. 使用charles模拟请求的返回数据，做数据mock
 
-21. 使用react-transition-group动画库做一些动画，具体读文档
+22. 使用react-transition-group动画库做一些动画，具体读文档
 
     ```react
     <CSSTransition in={this.state.show} // 控制显示
@@ -196,7 +204,7 @@
     }
     ```
 
-22. TransitionGroup 实现列表动画
+23. TransitionGroup 实现列表动画
 
     ```react
     <TransitionGroup>
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,94 +1,124 @@
 import React, {Component, Fragment} from 'react'
 import axios from 'axios'
 import TodoItem from './TodoItem'
+import {Input, Button, List} from 'antd'
+import 'antd/dist/antd.css'
 import './style.css'
 
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+]
+
 class TodoList extends Component {
-
-  // constructor 在组件创建时执行
-  constructor (props) {
-    super(props)
-
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.handleKeyUp = this.handleKeyUp.bind(this)
-    this.handleItemClick = this.handleItemClick.bind(this)
-    // 数据写在state中
-    this.state = {
-      inputValue: '',
-      list: []
-    }
-  }
-
-  handleInputChange (e) {
-    // const value = e.target.value
-    const value = this.input.value // 使用ref获取dom操作
-    this.setState(() => ({
-      inputValue: value
-    }))
-  }
-
-  handleKeyUp (e) {
-    if (e.keyCode === 13 && e.target.value !== '') {
-      this.setState((prevState) => ({
-        list: [...prevState.list, prevState.inputValue],
-        inputValue: ''
-      }))
-    }
-  }
-
-  handleItemClick (index) {
-    this.setState((prevState) => {
-      const list = [...prevState.list]
-      list.splice(index, 1)
-      return {
-        list
-      }
-    })
-  }
-
-  getListItems () {
-    // 父子组件的概念
-    // 父组件通过的形式向子组件传值
-    return this.state.list.map((value, index) => {
-      return (
-        <TodoItem key={index}
-                  content={value}
-                  index={index}
-                  deleteFunction={this.handleItemClick}
-        />
-      )
-    })
-  }
-
-  componentDidMount () {
-    axios.get('/api/todolist').then((res) => {
-      console.log(res.data)
-      this.setState(() => ({
-        list: [...res.data]
-      }))
-    })
-  }
-
   render () {
     return (
-      <Fragment>
-        {/*这是注释*/}
-        <label htmlFor={'insertArea'}>请输入内容：</label>
-        <input className={'input'}
-               id={'insertArea'}
-               value={this.state.inputValue}
-               onChange={this.handleInputChange}
-               onKeyUp={this.handleKeyUp}
-               ref={(input) => {
-                 this.input = input
-               }}
+      <div style={{ marginTop: '10px', marginLeft: '10px' }}>
+        <div>
+          <Input placeholder={'todo'} style={{ width: '300px', marginRight: '10px' }}/>
+          <Button type="primary">提交</Button>
+        </div>
+        <List style={{ marginTop: 10, width: 300 }}
+              bordered
+              dataSource={data}
+              renderItem={item => (
+                <List.Item>{item}</List.Item>
+              )}
         />
-        <ul>
-          {this.getListItems()}
-        </ul>
-      </Fragment>
+      </div>
     )
   }
 }
+
+// class TodoList extends Component {
+//
+//   // constructor 在组件创建时执行
+//   constructor (props) {
+//     super(props)
+//
+//     this.handleInputChange = this.handleInputChange.bind(this)
+//     this.handleKeyUp = this.handleKeyUp.bind(this)
+//     this.handleItemClick = this.handleItemClick.bind(this)
+//     // 数据写在state中
+//     this.state = {
+//       inputValue: '',
+//       list: []
+//     }
+//   }
+//
+//   handleInputChange (e) {
+//     // const value = e.target.value
+//     const value = this.input.value // 使用ref获取dom操作
+//     this.setState(() => ({
+//       inputValue: value
+//     }))
+//   }
+//
+//   handleKeyUp (e) {
+//     if (e.keyCode === 13 && e.target.value !== '') {
+//       this.setState((prevState) => ({
+//         list: [...prevState.list, prevState.inputValue],
+//         inputValue: ''
+//       }))
+//     }
+//   }
+//
+//   handleItemClick (index) {
+//     this.setState((prevState) => {
+//       const list = [...prevState.list]
+//       list.splice(index, 1)
+//       return {
+//         list
+//       }
+//     })
+//   }
+//
+//   getListItems () {
+//     // 父子组件的概念
+//     // 父组件通过的形式向子组件传值
+//     return this.state.list.map((value, index) => {
+//       return (
+//         <TodoItem key={index}
+//                   content={value}
+//                   index={index}
+//                   deleteFunction={this.handleItemClick}
+//         />
+//       )
+//     })
+//   }
+//
+//   componentDidMount () {
+//     axios.get('/api/todolist').then((res) => {
+//       console.log(res.data)
+//       this.setState(() => ({
+//         list: [...res.data]
+//       }))
+//     })
+//   }
+//
+//   render () {
+//     return (
+//       <Fragment>
+//         {/*这是注释*/}
+//         <label htmlFor={'insertArea'}>请输入内容：</label>
+//         <input className={'input'}
+//                id={'insertArea'}
+//                value={this.state.inputValue}
+//                onChange={this.handleInputChange}
+//                onKeyUp={this.handleKeyUp}
+//                ref={(input) => {
+//                  this.input = input
+//                }}
+//         />
+//         <ul>
+//           {this.getListItems()}
+//         </ul>
+//       </Fragment>
+//     )
+//   }
+// }
 
 export default TodoList
